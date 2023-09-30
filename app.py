8d120ed7d7d8c8072b0deef8bcc8698c7d849eb4
Prasadchaskar <chaskarprasad2000@gmail.com>
2023-09-30 10:57:51 +0530

    Data ingestion

---
+++ app.py
@@ -0,0 +1,17 @@
+from src.mlproject.logger import logging
+from src.mlproject.exception import CustomException
+import sys
+from src.mlproject.components.data_ingestion import DataIngestion
+from src.mlproject.components.data_ingestion import DataIngestionConfig
+
+if __name__ == "__main__":
+    logging.info("Logging working fine...")
+    
+    try:
+        # data_ingestion_config = DataIngestionConfig()
+        data_ingestion = DataIngestion()
+        data_ingestion.initiate_data_ingestion()
+
+    except Exception as e:
+        logging.info("Custom Exception")
+        raise CustomException(e,sys)
