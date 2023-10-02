d7520f3eeb4e2648c87c7d88bf2206ec6260a72f
Prasadchaskar <chaskarprasad2000@gmail.com>
2023-10-02 19:51:19 +0530

    Model Training

--- app.py
+++ app.py
@@ -1,11 +1,9 @@
 from src.mlproject.logger import logging
 from src.mlproject.exception import CustomException
 import sys
-from src.mlproject.components.data_ingestion import DataIngestion
-from src.mlproject.components.data_ingestion import DataIngestionConfig
-from src.mlproject.components.data_transformation import DataTransformation
-from src.mlproject.components.data_transformation import DataTransformationConfig
-
+from src.mlproject.components.data_ingestion import DataIngestion,DataIngestionConfig
+from src.mlproject.components.data_transformation import DataTransformation,DataTransformationConfig
+from src.mlproject.components.model_tranier import ModelTrainerConfig,ModelTrainer
 if __name__ == "__main__":
     logging.info("Logging working fine...")
     
@@ -16,7 +14,11 @@ if __name__ == "__main__":
 
         data_transformation_config = DataTransformationConfig()
         data_transformation = DataTransformation()
-        data_transformation.initiate_data_transormation(train_data_path,test_data_path)
+        train_array,test_array,_ = data_transformation.initiate_data_transormation(train_data_path,test_data_path)
+
+        #Model Training
+        model_trainer = ModelTrainer()
+        print(model_trainer.initiate_model_trainer(train_array,test_array))
     except Exception as e:
         logging.info("Custom Exception")
         raise CustomException(e,sys)
